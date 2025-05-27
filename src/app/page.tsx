d7b20bdb827883542b8a7a138d518dfb71e444bb
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-05-27 17:57:18 -0300

    [ FEATURE ] Adding fonts.
    - Adding font Nunito sans;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <p>Hello World!</p>
+    <div className="font-[family-name:var(--font-nunito-sans)] font-semibold">
+      <p className="text-3xl">Nunito Semibold</p>
+      <p className="text-3xl font-bold">Nunito Bold</p>
     </div>
   );
 }
